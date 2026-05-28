4158e6bab04098d3c336da31b853f1fd58537810
BSTech <bstech.solutions@outlook.com>
2026-05-28 12:20:13 -0300

    fix: guard telescope provider para produção

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -11,9 +11,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
+        if ($this->app->environment('local')) {
+            if (class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+                $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            }
+            if (class_exists(\App\Providers\TelescopeServiceProvider::class)) {
+                $this->app->register(\App\Providers\TelescopeServiceProvider::class);
+            }
         }
     }
 
